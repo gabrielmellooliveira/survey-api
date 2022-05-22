85a6d7916d2658c3d6938e4aeefe011e8dbf98d3
Gabriel Mello de Oliveira <gabrielmello3011@gmail.com>
2022-05-22 15:31:33 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,16 +1,17 @@
-import { MongoClient } from 'mongodb';
+import { Collection, MongoClient } from 'mongodb';
 
 export const MongoHelper = {
   mongoClient: null as MongoClient,
 
   async connect(uri: string): Promise<void> {
-    this.mongoClient = await MongoClient.connect(this.uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    this.mongoClient = await MongoClient.connect(uri);
   },
 
   async disconnect(): Promise<void> {
     await this.mongoClient.close();
+  },
+
+  getCollection(name: string): Collection {
+    return this.mongoClient.db().collection(name);
   },
 };
